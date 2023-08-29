7dedee987fb852f97d24d6667acf5a43c14625ed
Gerardo Martínez <74314228+Gerardo-MtzD@users.noreply.github.com>
2023-08-29 14:58:33 -0600

    Add files via upload

--- BLE.py
+++ BLE.py
@@ -1,11 +1,166 @@
 import asyncio
-from bleak import BleakClient
+from bleak import BleakClient, discover, BLEDevice
+from aioconsole import ainput
+
+from datetime import datetime
+from typing import Callable, Any
+
 
 
 async def ble_connection(address: str, uuid: str) -> None:
-    "Async function in charge of making and mantaining BLE connection between Android and ESP32"
-    if address == "" or uuid == "":
-        return
-    async with BleakClient(address) as client:
-        model_number = await client.read_gatt_char(uuid)
-        print("Model Number: {0}".format("".join(map(chr, model_number))))
+    """Async function in charge of making and mantaining BLE connection between Android and ESP32
+        + NOT USED - just for debugging purposes"""
+    for _ in range(10):
+        print('connecting...')
+        if address == "" or uuid == "":
+            pass
+        else:
+            async with BleakClient(address) as client:
+                model_number = await client.read_gatt_char(uuid)
+                print("Model Number: {0}".format("".join(map(chr, model_number))))
+            break
+        await asyncio.sleep(0.1)
+
+class Connection:
+    client: BleakClient = None
+
+    def __init__(self,
+                 loop: asyncio.AbstractEventLoop,
+                 uuid: str, address: str,
+                 read_char: str, write_char: str,
+                 dump_size: int = 256,):
+        self.loop = loop
+        self.uuid = uuid
+        self.address = address
+        self.read_char = read_char
+        self.write_char = write_char
+        self.dump_size = dump_size
+
+        self.connected = False
+        self.connected_device = None
+
+        self.rx_data = []
+        self.rx_timestamps = []
+        self.rx_delays = []
+
+    def on_disconnect(self, client: BleakClient, future: asyncio.Future) -> None:
+        """Protocol on disconnect of BLE"""
+        self.connected = False
+        print(f"Disconnected from {self.connected_device.name}!")
+
+    async def cleanup(self) -> None:
+        """Notification and client cleanup"""
+        if self.client:
+            await self.client.stop_notify(self.read_char)
+            await self.client.disconnect()
+
+    async def manager(self) -> None:
+        """Connection manager. Makes sure that client exists and establishes connection"""
+        print("Starting connection manager.")
+        while True:
+            if self.client:
+                await self.connect()
+                await asyncio.sleep(1.0)
+            else:
+                await self.select_device()
+                await asyncio.sleep(15.0)
+
+    async def connect(self) -> None:
+        """Connection mainframe between app and ESP32.
+            + Contains select_device() and discover_device() as alternate protocol if desired object is not found"""
+        print('in connect')
+        if self.connected:
+            return
+        try:
+            print('trying to connect to device...')
+            await self.client.connect()
+            print(f"connection status: {self.connected}")
+            self.connected = await self.client.is_connected()
+            if self.connected:
+                self.client.set_disconnected_callback(self.on_disconnect)
+                await self.client.start_notify(self.read_char,self.notification_handler)
+                while True:
+                    if not self.connected:
+                        break
+                    await asyncio.sleep(3.0)
+            else:
+                print(f"Failed to connect to {self.connected_device.name}")
+        except Exception as e:
+            print(e)
+
+    async def select_device(self, uuid: str = None, address: str = None) -> None:
+        print(f"Bluetooh LE hardware warming up...{datetime.now()}")
+        await asyncio.sleep(2.0)  # Wait for BLE to initialize.
+        print(datetime.now())
+        if self.uuid and self.address:
+            self.connected_device = [uuid, address]
+            print('in self.client')
+            while True:
+                self.client = BleakClient(address, loop=self.loop)
+                print(self.client)
+                if self.client:
+                    break
+                else:
+                    print('device not found')
+                    await asyncio.sleep(1.0)
+        else:
+            await self.discover_device()
+
+    async def discover_device(self) -> None:
+        devices = await asyncio.create_task(discover())
+        print("Please select device: ")
+        for i, device in enumerate(devices):
+            print(f"{i}: {device.name}")
+        response = -1
+        while True:
+            response = await ainput("Select device: ")
+            try:
+                response = int(response.strip())
+            except:
+                print("Please make valid selection.")
+
+            if response > -1 and response < len(devices):
+                break
+            else:
+                print("Please make valid selection.")
+
+        print(f"Connecting to {devices[response].name}")
+        self.connected_device = devices[response]
+        self.client = BleakClient(devices[response].address, loop=self.loop)
+
+    def record_time_info(self) -> None:
+        present_time = datetime.now()
+        self.rx_timestamps.append(present_time)
+        self.rx_delays.append((present_time - self.last_packet_time).microseconds)
+        self.last_packet_time = present_time
+
+    def clear_lists(self):
+        self.rx_data.clear()
+        self.rx_delays.clear()
+        self.rx_timestamps.clear()
+
+    def notification_handler(self, sender: str, data: Any):
+        self.rx_data.append(int.from_bytes(data, byteorder="big"))
+        self.record_time_info()
+        if len(self.rx_data) >= self.dump_size:
+            self.data_dump_handler(self.rx_data, self.rx_timestamps, self.rx_delays)
+            self.clear_lists()
+
+async def communication_manager(connection: Connection, write_char: str, read_char: str):
+    """In charge of sending and receiving information
+        + IMPORTANT to pair write and read characteristics between App and ESP32"""
+    while True:
+        if connection.client and connection.connected:
+            input_str = ""
+            bytes_to_send = bytearray(map(ord, input_str))
+            await connection.client.write_gatt_char(write_char, bytes_to_send)
+            print(f"Sent: {input_str}")
+            msg_read = await connection.client.read_gatt_char(read_char)
+            print(msg_read)
+        else:
+            await asyncio.sleep(2.0)
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from kivymd.app import MDApp
+from kivy.app import async_runTouchApp
+from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 import asyncio
 from CircularProgressBar import CircularProgressBar
-from CustomSpeed import Speedometer
+from BLE import Connection, communication_manager
 
 
-#kivy.version('1.9.0')
+# kivy.version('1.9.0')
 
 from BLE import ble_connection
 from Speedometer import SpeedMeter
 
 
 
-loop = asyncio.new_event_loop() # Here
-asyncio.set_event_loop(loop) # Here
+#loop = asyncio.new_event_loop() # Here
+#asyncio.set_event_loop(loop) # Here
 
-ADDRESS, UUID = "", "4295124f-50e1-4d13-bf04-c5dea961600b"
+#ADDRESS, UUID = "78:21:84:9D:37:10", "4295124f-50e1-4d13-bf04-c5dea961600b"
+ADDRESS, UUID = "", ""
 
 class MainWindow(Screen): pass
 
@@ -28,11 +31,19 @@ class WindowManager(ScreenManager): pass
 
 class Main(MDApp):
     def build(self):
-        # setting design for application
-        # widget development specifications on design.kv
+        """setting design for application
+        widget development specifications on design.kv"""
         self.theme_cls.theme_style = 'Light'
         self.theme_cls.primary_palette = 'Orange'
         return Builder.load_file(filename='design.kv')
+
+    async def launch_app(self):
+        """Asyncronous function for kivy app start"""
+        await self.async_run(async_lib='asyncio')
+
+    async def start(self):
+        """Asyncronous app making sure start is awaited as coroutine"""
+        (done, pending) = await asyncio.wait({self.launch_app()},return_when='FIRST_COMPLETED')
 
     def on_start(self):
         self.speedmeter = self.root.get_screen('secondary_window').ids.speed
@@ -44,16 +55,14 @@ class Main(MDApp):
 
     def connect_ble(self, touch: bool) -> None:
         """Function handling BLE connection between App and ESP32"""
+        print('in connect_ble')
         Button = self.root.get_screen('main_window').ids.ble_button
         if touch:
             try:
-                #asyncio.ensure_future(ble_connection(ADDRESS,UUID))
-                #loop.run_forever()
-                loop.run_until_complete(ble_connection(ADDRESS,UUID))
-            finally:
-                print("Closing Loop")
-                loop.close()
-            print('in connect_ble')
+                asyncio.create_task(run_BLE(self))
+                #asyncio.create_task(debug_BLE(self))
+            except Exception as e:
+                print(e)
 
     def switch_state(self, _, value: bool) -> None:
         """Switch state for adaptive mode switch. When in adaptive mode, the slider is disabled"""
@@ -65,7 +74,8 @@ class Main(MDApp):
 
 
     def slider_slide(self, _, value: int) -> None:
-        """Sends percentage of assistance wanted to ESP32"""
+        """Sends percentage of assistance wanted to ESP32
+        In automatic mode: Use BATTERY and ACCELEROMETER values to determine percentage of use"""
         print(value)
         self.update_speed_value(value)
         self.update_battery_value(value)
@@ -81,7 +91,7 @@ class Main(MDApp):
             +READ FROM BLE TO UPDATE VALUE
 
         *for debugging purposes it shows the value from slider"""
-        if value > 2*self.speedmeter.end_value/(3.6):
+        if value > 2*self.speedmeter.end_value/3.6:
             pass
         else:
             self.speedmeter.set_value = value - 25
@@ -99,13 +109,42 @@ class Main(MDApp):
         self.circle_bar.set_value = value
         self.circle_bar.text = f'{value}%'
 
+async def debug_BLE(app: MDApp) -> None:
+    app.root.get_screen('main_window').ids.spinner.active = True
+    for i in range(10):
+        print(f"Other coroutine -> {i}")
+        await asyncio.sleep(1)
+    app.root.current = 'secondary_window'
 
-
-
-def run_app():
-    Main().run()
-    #asyncio.run(ble_connection(ADDRESS, UUID))
+async def run_BLE(app: MDApp) -> None:
+    """Asyncronous connection protocol for BLE"""
+    app.root.get_screen('main_window').ids.spinner.active = True
+    print('in run_BLE')
+    read_char = ""
+    write_char = ""
+    connection = Connection(loop,
+                            UUID,
+                            ADDRESS,
+                            read_char,
+                            write_char)
+    try:
+        asyncio.ensure_future(connection.manager())
+        asyncio.ensure_future(communication_manager(connection,write_char=write_char,read_char=read_char))
+        if connection.connected:
+            app.root.current = 'secondary_window'
+        #loop.run_forever()
+    finally:
+        print("Disconnecting...")
+        #loop.run_until_complete(connection.cleanup())
 
 if __name__=='__main__':
-    run_app()
+    async def mainThread():
+        """Creating main thread for asyncronous task definition"""
+        BikeApp = Main()
+        a = asyncio.create_task(BikeApp.start())
+        (done,pending) = await asyncio.wait({a},return_when='FIRST_COMPLETED')
+
+    loop = asyncio.get_event_loop()
+    asyncio.run(mainThread())
+
 
